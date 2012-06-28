f682f9a1b7f323d5c17540c2eca67cf6d4ec3350
Harisuman Naik <Harisuman.Naik@wi1.wiso.uni-erlangen.de>
2012-06-28 09:30:58 -0500

    changed javascript to make it more sophisticated towards character checks

--- twitter_post/twitter_post.js
+++ twitter_post/twitter_post.js
@@ -3,12 +3,30 @@
  * of characters remaining to achieve the max length
  */
 Drupal.behaviors.twitter_post = function (context) {
-  $("#twitter-textfield", context).keyup(function() {
-    var charsLeft = (120 - $(this).val().length);
+   var titleOffset = 0;
+
+   $("#edit-title", context).keyup(function() {
+     if($("#twitter-textfield").val().indexOf("!title") != -1) {
+       titleOffset = $("#edit-title").val().length; 
+       var charsLeft = (120 - titleOffset - $("#twitter-textfield").val().length +6);
+       var descDiv = $("#twitter-textfield").next();
+       $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining");
+       if (charsLeft < 0) {
+         $("#edit-title").val($("#edit-title").val().substring(0, 120 - $("#twitter-textfield").val().length + 5));
+       }
+     }
+   });
+
+   $("#twitter-textfield", context).keyup(function() {
+    var charsLeft = (120 - $(this).val().length - titleOffset + 6);
     var descDiv = $(this).next();
     $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining");
     if (charsLeft < 0) {
-      $(descDiv).addClass("negative");
+      if($("#twitter-textfield").val().indexOf("!title") != -1) {
+        $("#twitter-textfield").val($("#twitter-textfield").val().substring(0, 120 - $("#edit-title").val().length + 6));
+      }else{
+        $("#twitter-textfield").val($("#twitter-textfield").val().substring(0, 120));
+      }
     } else {
       $(descDiv).removeClass("negative");
     }
