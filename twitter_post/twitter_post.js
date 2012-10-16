88f95621686ae0180253ebe1b5de17c6093ed754
Harisuman Naik <Harisuman.Naik@wi1.wiso.uni-erlangen.de>
2012-10-16 16:28:02 -0500

    changed twitter logic and added color warning

--- twitter_post/twitter_post.js
+++ twitter_post/twitter_post.js
@@ -4,38 +4,46 @@
  */
 Drupal.behaviors.twitter_post = function (context) {
    var titleOffset = 0;
+   var max_length = 105;
 
-/*   $("#edit-title", context).keyup(function() {
+   $("#edit-title", context).keyup(function() {
      if($("#twitter-textfield").val().indexOf("!title") != -1) {
-       titleOffset = $("#edit-title").val().length; 
-       var charsLeft = (120 - titleOffset - $("#twitter-textfield").val().length +6);
+       titleOffset = $("#edit-title").val().length - 6; /* length of !title*/
+       var realLength = $("#twitter-textfield").val().length + titleOffset;
+       var charsLeft = (max_length - realLength);
+       
        var descDiv = $("#twitter-textfield").next();
-       $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining. Remove !title in twitter field for more characters");
+       $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining " + realLength);
+
        if (charsLeft < 0) {
-         $("#edit-title").val($("#edit-title").val().substring(0, 120 - $("#twitter-textfield").val().length + 5));
+        $("#twitter-textfield").val($("#twitter-textfield").val().substring(0, max_length - titleOffset));
+        $(descDiv).html("<strong>" + 0 + "</strong> characters remaining. Maximum length reached");
+        $(descDiv).css("color","#FF0000");   
+       } else {
+         $(descDiv).removeAttr("style");
        }
      }
-   });*/
+   });
 
    $("#twitter-textfield", context).keyup(function() {
     if($("#twitter-textfield").val().indexOf("!title") != -1) {
-       titleOffset = $("#edit-title").val().length - 6; 
+       titleOffset = $("#edit-title").val().length - 6; /* length of !title*/
     }else{
        titleOffset = 0;
     }
+    var realLength = $(this).val().length + titleOffset;
+    var charsLeft = (max_length - realLength);
 
-    var charsLeft = (120 - $(this).val().length - titleOffset);
     var descDiv = $(this).next();
     $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining");
-    if (charsLeft <= 0 || $(this).val().length == 120) {
-      if($("#twitter-textfield").val().indexOf("!title") != -1) {
-        $("#twitter-textfield").val($("#twitter-textfield").val().substring(0, 120 - titleOffset));
-        $(descDiv).html("<strong>" + charsLeft + "</strong> additional characters possible. Remove !title in twitter field for more characters");
-      }else{
-        $("#twitter-textfield").val($("#twitter-textfield").val().substring(0, 120));
-      }
+
+    if (charsLeft < 0) {
+      $("#twitter-textfield").val($("#twitter-textfield").val().substring(0, max_length - titleOffset));
+      $(descDiv).html("<strong>" + 0 + "</strong> characters remaining. Maximum length reached");
+      $(descDiv).css("color","#FF0000");
     } else {
       $(descDiv).removeClass("negative");
+      $(descDiv).removeAttr("style");
     }
   });
 
