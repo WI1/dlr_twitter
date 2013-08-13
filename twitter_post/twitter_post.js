1be101e37dc83f97c9207a0dc5fea87ac15d25ca
Harisuman Naik <Harisuman.Naik@wi1.wiso.uni-erlangen.de>
2013-08-13 06:47:01 -0500

    recreated twitter module version 5 with dlr code

--- twitter_post/twitter_post.js
+++ twitter_post/twitter_post.js
@@ -37,7 +37,10 @@ Drupal.behaviors.twitter_post = function (context) {
      }
    });
 
-   $("#twitter-textfield", context).keyup(function() {
+
+
+  $("#twitter-textfield", context).keyup(function() {
+ 
     titleOffset = 0;
     if($("#twitter-textfield").val().toLowerCase().indexOf("!titel") != -1) {
        titleOffset = $("#edit-title").val().length - 6; /* length of !titel*/
@@ -66,20 +69,34 @@ Drupal.behaviors.twitter_post = function (context) {
     }
   });
 
-  if (!$("#twitter-toggle").attr("checked")) {
-    $("#twitter-textfield-wrapper").hide();
+
+
+
+    if (!$("#twitter-toggle").attr("checked")) {
+    $("#twitter-textfield").hide();
+//    $(".form-item-twitter-status").hide();
   }
 
   $("#twitter-toggle").bind("click", function() {
     if ($("#twitter-toggle").attr("checked")) {
-      $("#twitter-textfield-wrapper").show();
-      
+      $("#twitter-textfield").show();
+      //$(".form-item-twitter-status").show();
+    
       if($("#twitter-textfield").val().toLowerCase().indexOf("!titel") != -1) {
          titleOffset = $("#edit-title").val().length - 6; /* length of !titel*/
       }
       var realLength = $("#twitter-textfield").val().length + titleOffset;
       var charsLeft = (max_length - realLength);
-      $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining");
+      
+      $(descDiv).html("Der hier eingegebene Text wird auf Twitter.com im Kanal @DemoScreen veröffentlicht, nachdem er zuvor durch den Hashtag #demografie sowie einen" 
+      + " TinyURL-Link zu diesem Beitrag erweitert wurde. Daher stehen Ihnen für den eigenen Text nur " + max_length + " Zeichen zur Verfügung. Fügen Sie in diesem Feld die Zeichenkette "
+      + "'!titel' ein, so wird diese vor der Übertragung zu Twitter durch den Inhalt des Eingabefeldes 'Titelzeile' ersetze. Wenn Sie dieses Eingabefeld leer lassen, "
+      + "wird ausschließlich der Inhalt aus dem Feld 'Titelzeile' als Nachricht für Twitter.com verwendet. <br><br> Noch <strong>" + charsLeft + "</strong> weitere Zeichen können zu Twitter übertragen werden");
+
+
+
+
+
       if (charsLeft < 0) {
         $(descDiv).html("Die Maximallänge für Twitter-Nachrichten wurde überschritten. Entfernen Sie mindestens <strong>" + -1*charsLeft +" </strong> Zeichen, ansonsten werden diese automatisch vom Ende abgeschnitten.");
         $(titleDescDiv).html("Die Maximallänge für Twitter-Nachrichten wurde überschritten. Entfernen Sie mindestens <strong>" + -1*charsLeft +"</strong> Zeichen, ansonsten werden diese automatisch vom Ende abgeschnitten.");        
@@ -89,15 +106,15 @@ Drupal.behaviors.twitter_post = function (context) {
         $(titleDescDiv).css("color","#FF0000");
         $(titleDescDiv).css("display","block");
         
-        } else {
-        $(descDiv).removeClass("negative");
-        $(descDiv).removeAttr("style");
-        $(titleDescDiv).removeAttr("style");
-        $(titleDescDiv).html("Enter title here"); 
-      }
-    }
-    else {
-      $("#twitter-textfield-wrapper").hide();
+      } else {
+          $(descDiv).removeClass("negative");
+          $(descDiv).removeAttr("style");
+          $(titleDescDiv).removeAttr("style");
+          $(titleDescDiv).html("Enter title here"); 
+      } 
+    } else {
+      //$(".form-item-twitter-status").hide();
+      $("#twitter-textfield").hide();
       $(descDiv).removeAttr("style");
       $(titleDescDiv).removeAttr("style");
       $(titleDescDiv).html("Enter title here"); 
